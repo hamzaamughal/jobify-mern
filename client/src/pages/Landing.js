0c925369ffa2cb54f1215b1ed6817a210f8d2101
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-01-24 13:02:35 +0500

    Structure for Dashboard Error and Register Page Added and Implement React-Router

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,12 +1,13 @@
-import logo from '../assets/images/logo.svg';
+import { Link } from 'react-router-dom';
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt='logo' className='logo' />
+        <Logo />
       </nav>
       <div className='container page'>
         <div className='info'>
@@ -18,7 +19,9 @@ const Landing = () => {
             repellat voluptatibus id accusamus! Eos explicabo voluptate
             repellendus nihil officiis? Cum.
           </p>
-          <button className='btn btn-hero'>Login/Register</button>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt='job-hunt' className='img main-img' />
       </div>
