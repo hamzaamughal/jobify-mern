b499299a5ba252f84f6f96dff2dfedeb738db1bb
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-01-24 21:47:09 +0500

    added actions and reducers for register

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,7 @@
 import React, { useContext, useReducer, createContext } from 'react';
 
+import { CLEAR_ALERT, DISPLAY_ALERT } from './action';
+
 import reducer from './reducer';
 
 const initialState = {
@@ -14,8 +16,20 @@ const AppContext = createContext();
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const displayAlert = () => {
+    dispatch({ type: DISPLAY_ALERT });
+    clearAlert();
+  };
+  const clearAlert = () => {
+    setTimeout(() => {
+      dispatch({ type: CLEAR_ALERT });
+    }, 3000);
+  };
+
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state, displayAlert }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,4 +1,22 @@
+import { CLEAR_ALERT, DISPLAY_ALERT } from './action';
+
 const reducer = (state, action) => {
+  if (action.type === DISPLAY_ALERT) {
+    return {
+      ...state,
+      showAlert: true,
+      alertText: 'Please provide all values!',
+      alertType: 'danger',
+    };
+  }
+  if (action.type === CLEAR_ALERT) {
+    return {
+      ...state,
+      showAlert: false,
+      alertText: '',
+      alertType: '',
+    };
+  }
   throw new Error(`Reducer not implemented for ${action.type}`);
 };
 
