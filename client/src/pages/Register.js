9b06b8cd5a43904aaca510943965742c9b6a9811
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-01-24 13:53:18 +0500

    ALert and FormRow Components Added

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Logo } from '../components';
+import { Alert, FormRow, Logo } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
 // global context and useNavigate later
 
@@ -8,6 +8,7 @@ const initialState = {
   email: '',
   password: '',
   isMember: true,
+  showAlert: false,
 };
 
 export default function Register() {
@@ -28,22 +29,29 @@ export default function Register() {
       <form className='form' onSubmit={onSubmit}>
         <Logo />
         <h3>Login</h3>
-
+        {values.showAlert && <Alert />}
         {/* name field */}
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-
-          <input
-            type='text'
-            value={values.name}
-            name='name'
-            onChange={handleChange}
-            className='form-input'
-          />
-        </div>
-
+        <FormRow
+          type='text'
+          name='name'
+          value={values.name}
+          handleChange={handleChange}
+          labelText='Name'
+        />
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+          labelText='Email'
+        />
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+          labelText='Password'
+        />
         <button type='submit' className='btn btn-block'>
           submit
         </button>
