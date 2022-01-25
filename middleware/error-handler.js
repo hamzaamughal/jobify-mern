3bb6542ae302d9b2ba708c0cdfb5a37a7c92e32d
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-01-25 18:32:28 +0500

    Added Error Handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -3,10 +3,23 @@ import { StatusCodes } from 'http-status-codes';
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
   const defaultError = {
-    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    message: 'Something went wrong, Try Again Later',
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || 'Something went wrong, Try Again Later',
   };
-  res.status(defaultError.statusCode).json({ msg: err });
+  if (err.name === 'ValidationError') {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    // defaultError.message = err.message;
+    defaultError.message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+  }
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.message = `${Object.keys(
+      err.keyValue
+    )} field has to be unique`;
+  }
+  res.status(defaultError.statusCode).json({ msg: defaultError.message });
 };
 
 export default errorHandlerMiddleware;
