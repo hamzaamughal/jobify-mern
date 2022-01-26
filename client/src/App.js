c88b9d01f82f1b7628c1681a2aeb13976b68dde7
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-01-26 18:30:24 +0500

    added structure for dashboard pages and Protected Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Dashboard, Landing, Register, Error } from './pages';
+import { Landing, Register, Error, ProtectedRoute } from './pages';
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+} from './pages/dashboard/index';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
+          <Route path='all-jobs' element={<AllJobs />} />
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
