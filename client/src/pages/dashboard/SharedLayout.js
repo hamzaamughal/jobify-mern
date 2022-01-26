371796a26fee544b98df6216456e014cfd846d25
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-01-26 18:46:18 +0500

    added Structure for Shared Layout

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,15 +1,25 @@
 import { Outlet, Link } from 'react-router-dom';
 import Wrapper from '../../assets/wrappers/SharedLayout';
+import { Navbar, BigSidebar, SmallSidebar } from '../../components';
+import { useAppContext } from '../../context/appContext';
 
 const SharedLayout = () => {
+  const { user } = useAppContext();
   return (
-    <Wrapper>
-      <nav>
-        <Link to='all-jobs'>all jobs</Link>
-        <Link to='add-job'>all jobs</Link>
-      </nav>
-      <Outlet />
-    </Wrapper>
+    <>
+      <Wrapper>
+        <main className='dashboard'>
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className='dashboard-page'>
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </>
   );
 };
 
