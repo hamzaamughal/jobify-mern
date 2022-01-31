c6fa6ada9455e0593567aecc1993290aa8a77096
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-01-31 12:01:44 +0500

    page button container added

---
+++ client/src/components/PageBtnContainer.js
@@ -0,0 +1,49 @@
+import { useAppContext } from '../context/appContext';
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
+import Wrapper from '../assets/wrappers/PageBtnContainer';
+
+const PageButtonContainer = () => {
+  const { numOfPages, page } = useAppContext();
+
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1;
+  });
+
+  const prevPage = () => {
+    console.log('prev page');
+  };
+  const nextPage = () => {
+    console.log('next page');
+  };
+
+  return (
+    <Wrapper>
+      <button className='prev-btn' onClick={prevPage}>
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+
+      <div className='btn-container'>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              type='button'
+              className={pageNumber === page ? 'pageBtn active' : 'pageBtn'}
+              key={pageNumber}
+              onClick={() => console.log(page)}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+
+      <button className='next-btn' onClick={nextPage}>
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
+};
+
+export default PageButtonContainer;
