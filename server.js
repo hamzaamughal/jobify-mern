ae8a1486451a64081f3429ea7e7dc8659d7ca1c5
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-02-18 16:41:18 +0500

    final commit

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 import morgan from 'morgan';
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize';
+
 //db and authenticateUser
 // MONGO_URI=mongodb+srv://jobify:jobify@jobify.eur9z.mongodb.net/Jobify?retryWrites=true&w=majority
 import connectDB from './db/connect.js';
@@ -23,13 +31,20 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/jobs', authenticateUser, jobRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build/index.html'));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
